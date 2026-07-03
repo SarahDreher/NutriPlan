965d79cdfac7b874ff8c9c2069e109eff320ac27
Dreher <Sarah.Dreher@hnu.de>
2026-07-03 13:48:01 +0200

    little visual changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v5';
+const CACHE = 'nutriplan-v6';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
