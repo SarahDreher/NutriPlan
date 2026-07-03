2b0fb6d4fefdfdb808fce6b1e1f0305b980caba3
Dreher <Sarah.Dreher@hnu.de>
2026-07-03 21:19:29 +0200

    photo upload updated

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v8';
+const CACHE = 'nutriplan-v9';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
