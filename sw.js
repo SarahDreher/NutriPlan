8829e4978c3db7b9f2e849a0778b829d1d27f040
Dreher <sarah.dreher@hnu.de>
2026-07-08 13:42:06 +0200

    fixes in fix meals

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v21';
+const CACHE = 'nutriplan-v22';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
