0a29b5f3ad797d0e6146a7f240bfd0edcd606a47
Dreher <sarah.dreher@hnu.de>
2026-07-08 14:31:44 +0200

    reduce to plan

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v25';
+const CACHE = 'nutriplan-v26';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
