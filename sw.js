120c7075179b8a84c9c6db54b898f025522f66d2
Dreher <sarah.dreher@hnu.de>
2026-07-08 12:58:43 +0200

    changes in fixed meals

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v20';
+const CACHE = 'nutriplan-v21';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
