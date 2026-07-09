dbb430e0bfc6741fefa94cae47c9267906268fe3
Dreher <sarah.dreher@hnu.de>
2026-07-09 07:03:20 +0200

    sort shopping list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v27';
+const CACHE = 'nutriplan-v29';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
