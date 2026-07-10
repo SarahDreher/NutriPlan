b4ef8637437c06b2f049b48f04bdee081d815fa0
Dreher <sarah.dreher@hnu.de>
2026-07-10 09:47:02 +0200

    adding incredients in shopping list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v29';
+const CACHE = 'nutriplan-v30';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
