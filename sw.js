42f1f56aea6c500f200a6ec520e1826838f8c411
Dreher <Sarah.Dreher@hnu.de>
2026-07-20 19:17:05 +0200

    plan history improved

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v34';
+const CACHE = 'nutriplan-v35';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
