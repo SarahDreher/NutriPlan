5cc553d263efffbe90fe11cbf3359ac71e893a38
Dreher <Sarah.Dreher@hnu.de>
2026-07-30 16:12:17 +0200

    additional items in Shopping list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v43';
+const CACHE = 'nutriplan-v44';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
