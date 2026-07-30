d47b7ff06323f939b59a3acaab10b698fe49dc62
Dreher <Sarah.Dreher@hnu.de>
2026-07-30 15:28:32 +0200

    adding aditional items to shopping list

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'nutriplan-v42';
+const CACHE = 'nutriplan-v43';
 const ASSETS = ['/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
